0089c74e0512c0c258293848035f8d258f49e54c
Paulo Ferreira <paulo.ferreira@ditech.com.br>
2021-08-17 19:51:16 -0300

    Passing selected photo

--- CameraFilter/Controllers/PhotosCollectionViewController.swift
+++ CameraFilter/Controllers/PhotosCollectionViewController.swift
@@ -8,9 +8,16 @@
 import Foundation
 import UIKit
 import Photos
+import RxSwift
 
 
 class PhotosCollectionViewController: UICollectionViewController {
+    
+    private let selectedPhotoSubject = PublishSubject<UIImage>()
+    
+    var selectedPhoto: Observable<UIImage> {
+        return selectedPhotoSubject.asObserver()
+    }
     
     private var images = [PHAsset]()
     
@@ -25,6 +32,26 @@ class PhotosCollectionViewController: UICollectionViewController {
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedAsset = self.images[indexPath.row]
+        PHImageManager.default().requestImage(for: selectedAsset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFit, options: nil) { [weak self] image, info in
+            
+            guard let info = info else { return }
+            
+            let isDegradedImage = info["PHImageResultIsDegradedKey"] as! Bool
+            
+            if !isDegradedImage {
+                
+                if let image = image {
+                    self?.selectedPhotoSubject.onNext(image)
+                    self?.dismiss(animated: true, completion: nil)
+                }
+                
+            }
+            
+        }
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
