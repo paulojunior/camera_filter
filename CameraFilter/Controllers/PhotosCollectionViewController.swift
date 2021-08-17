ba8abc4af1c815fcbd084ff70db300f49400a54e
Paulo Ferreira <paulo.ferreira@ditech.com.br>
2021-08-17 19:42:56 -0300

    Displaying Photos from Photo Library

--- CameraFilter/Controllers/PhotosCollectionViewController.swift
+++ CameraFilter/Controllers/PhotosCollectionViewController.swift
@@ -19,6 +19,31 @@ class PhotosCollectionViewController: UICollectionViewController {
         populatePhotos()
     }
     
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as? PhotoCollectionViewCell else {
+            fatalError("PhotoCollectionViewCell is not found")
+        }
+        
+        let asset = self.images[indexPath.row]
+        let manager = PHImageManager.default()
+        
+        manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil) { image, _ in
+            DispatchQueue.main.async {
+                cell.photoImageView.image = image
+            }
+        }
+        
+        return cell
+    }
     
     private func populatePhotos() {
         PHPhotoLibrary.requestAuthorization() { [weak self] status in
@@ -32,11 +57,11 @@ class PhotosCollectionViewController: UICollectionViewController {
                 }
                 
                 self?.images.reverse()
-                //self?.collectionView.reloadData()
-            } else {
                 
+                DispatchQueue.main.async {
+                    self?.collectionView.reloadData()
+                }
             }
-            
         }
     }
 }
---
+++ CameraFilter/PhotoColllectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoColllectionViewCell.swift
+//  CameraFilter
+//
+//  Created by Junior Ferreira on 17/08/21.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+}
